e478143415eccebb325d22e6d86f6da0be886481
buichianhgithub <chianh.bui@georgebrown.ca>
2019-10-21 09:23:46 -0400

    add src, webcontent folder, delete META-INF

---
+++ WebContent/login.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="login" method="post">
+		<table>
+			<tr>
+				<td>Username: </td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				
+				<td><button type="submit" name="login">Login</button></td>
+				<td><button name="register">Register</button></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
---
+++ src/gbc/assignment/servlet/AuthenticationServlet.java
@@ -0,0 +1,63 @@
+package gbc.assignment.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import gbc.assignment.connection.DatabaseAccess;
+
+@WebServlet(name = "s1", urlPatterns = { "/login" })
+public class AuthenticationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public AuthenticationServlet() {
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		try {
+			Connection conn = DatabaseAccess.connectDataBase();
+			if (conn != null) {
+				PreparedStatement preparedStatement = conn
+						.prepareStatement("select * from users where email = ? and password = ?");
+
+				preparedStatement.setString(1, username);
+				preparedStatement.setString(2, password);
+				System.out.println(preparedStatement);
+				ResultSet rs = preparedStatement.executeQuery();
+				if (rs.next()) {
+					String fName = rs.getString("firstname");
+					HttpSession session = request.getSession();
+					session.setAttribute("first_name", fName);
+					response.sendRedirect("dashboard.jsp");
+				}
+				else {
+					PrintWriter pw = response.getWriter();
+					pw.write("Invalid Credentials");
+				}
+			}
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
